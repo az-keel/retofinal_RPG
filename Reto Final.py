13afc9825bc58202b656cb553e759038a5b55069
unknown <garzavargas2711@gmail.com>
2023-03-29 10:58:13 -0600

    Intente arreglar el codigo

--- Reto Final.py
+++ Reto Final.py
@@ -1,5 +1,32 @@
 from personajes import *
+from pociones import * 
+from LogicaJuego import * 
+from personajes import*
+from pociones import *
 import random
+class Player:
+    def __init__(self, name, player_type):
+        self.name = name
+        self.player_type = player_type
+        self.health = 100
+        self.attack_points = 10
+        self.defense_points = 5
+        self.potions = {"attack": 2, "defense": 2}
+
+    def attack(self, enemy):
+        damage = self.attack_points - enemy.defense_points
+        enemy.health -= damage
+
+    def use_potion(self, potion_type):
+        if self.potions[potion_type] > 0:
+            if potion_type == "attack":
+                self.attack_points += 5
+            elif potion_type == "defense":
+                self.defense_points += 5
+            self.potions[potion_type] -= 1
+            print(f"{self.name} used a {potion_type} potion. New {potion_type} points: {self.attack_points if potion_type == 'attack' else self.defense_points}")
+        else:
+            print(f"{self.name} does not have any {potion_type} potions.")
 
 def main():
     print("¡Bienvenido al juego de rol!")
