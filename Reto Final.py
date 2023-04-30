e808c310dcc3a31d6b87cda26fb81608cf494317
unknown <garzavargas2711@gmail.com>
2023-04-29 18:07:58 -0600

    Se termino el desarollo del juego

--- Reto Final.py
+++ Reto Final.py
@@ -1,58 +1,223 @@
 import random
 
-# Definimos las palabras base para cada nivel
-palabras_faciles = ['tina', 'piso', 'ropa', 'mesa']
-palabras_intermedias = ['ambulante', 'alfombras', 'biologico', 'computado']
-palabras_avanzadas = ['microondas', 'perpendicular', 'revolucionario', 'trascendencia']
+class Character:
+    def __init__(self, name, health, attack, defense, level, exp):
+        self.name = name
+        self.health = health
+        self.attack = attack
+        self.defense = defense
+        self.level = level
+        self.exp = exp
 
-# Definimos una función que mostrará la palabra base con las letras adivinadas por el usuario
-def mostrar_palabra(palabra_base, letras_adivinadas):
-    palabra_mostrada = ''
-    for letra in palabra_base:
-        if letra in letras_adivinadas:
-            palabra_mostrada += letra
+    def take_damage(self, damage):
+        self.health -= damage
+        if self.health < 0:
+            self.health = 0
+
+    def is_dead(self):
+        return self.health == 0
+
+    def gain_exp(self, exp):
+        self.exp += exp
+        while self.exp >= self.level * 100:
+            self.exp -= self.level * 100
+            self.level_up()
+
+    def level_up(self):
+        self.level += 1
+        self.health += 10
+        self.attack += 5
+        self.defense += 5
+        print(f"{self.name} has leveled up! Now at level {self.level}.")
+
+class Mage(Character):
+    def __init__(self):
+        super().__init__("Mage", 70, 20, 5, 1, 0)
+
+    # Habilidad especial: lanzar un hechizo poderoso
+    def cast_spell(self, enemy):
+        damage = self.attack * 2
+        enemy.take_damage(damage)
+        print(f"You cast a powerful spell dealing {damage} damage to the {enemy.name}!")
+
+class Knight(Character):
+    def __init__(self):
+        super().__init__("Knight", 100, 15, 10, 1, 0)
+
+    # Habilidad especial: fortalecerse temporalmente
+    def fortify(self):
+        self.attack *= 1.5
+        self.defense *= 1.5
+        print("You have fortified your attack and defense for the next turn!")
+
+class Assassin(Character):
+    def __init__(self):
+        super().__init__("Assassin", 80, 25, 8, 1, 0)
+
+    # Habilidad especial: ataque crítico con mayor probabilidad de golpe crítico
+    def critical_strike(self, enemy):
+        crit_chance = 0.5
+        if random.random() < crit_chance:
+            damage = self.attack * 3
+            print(f"Critical strike! You dealt {damage} damage to the {enemy.name}!")
         else:
-            palabra_mostrada += '-'
-    return palabra_mostrada
+            damage = self.attack
+            print(f"You dealt {damage} damage to the {enemy.name}.")
+        enemy.take_damage(damage)
 
-# Definimos una función que valida que la letra ingresada por el usuario sea única
-def validar_letra(letra, letras_adivinadas):
-    if letra in letras_adivinadas:
-        print('Ya ingresaste esa letra, intenta con otra.')
-        return False
+class Tank(Character):
+    def __init__(self):
+        super().__init__("Tank", 120, 10, 15, 1, 0)
+
+    # Habilidad especial: provocar al enemigo, forzándolo a atacar solo al tanque
+    def provoke(self, enemy):
+        enemy.attack -= 5
+        enemy.defense -= 5
+        print(f"You have provoked the {enemy.name}, lowering its attack and defense!")
+
+class Enemy(Character):
+    def __init__(self, name, health, attack, defense, level):
+        super().__init__(name, health, attack, defense, level, 0)
+
+def print_stats(character):
+    print(f"{character.name}: Health {character.health}, Attack {character.attack}, Defense {character.defense}, Level {character.level}, EXP {character.exp}")
+
+def choose_character():
+    print("Choose your character:")
+    print("1. Mage")
+    print("2. Knight")
+    print("3. Assassin")
+    print("4. Tank")
+
+    choice = int(input())
+    if choice == 1:
+        return Mage()
+    elif choice == 2:
+        return Knight()
+    elif choice == 3:
+        return Assassin()
+    elif choice == 4:
+        return Tank()
     else:
+        print("Invalid choice, try again.")
+        return choose_character()
+
+def create_enemy(player_level):
+    enemy_types = [
+        ("Goblin", 50, 10, 5, 1),
+        ("Orc", 80, 15, 10, 3),
+        ("Troll", 100, 20, 15, 5)
+    ]
+    enemy = random.choice(enemy_types)
+    level_diff = player_level - enemy[4]
+    enemy = Enemy(*enemy)
+    for _ in range(level_diff):
+        enemy.level_up()
+    return enemy
+
+def special_ability_menu(player):
+    print("Choose a special ability:")
+    if isinstance(player, Mage):
+        print("1. Powerful spell")
+    elif isinstance(player, Knight):
+        print("1. Fortify")
+    elif isinstance(player, Assassin):
+        print("1. Critical strike")
+    elif isinstance(player, Tank):
+        print("1. Provoke")
+
+    choice = int(input())
+    if choice == 1:
         return True
-
-# Definimos la función principal del juego
-def jugar_ahorcado():
-    # Pedimos al usuario que elija el nivel a jugar
-    nivel = ''
-    while nivel not in ['facil', 'intermedio', 'avanzado']:
-        nivel = input('Elige un nivel para jugar (facil, intermedio, avanzado): ').lower()
-    if nivel == 'facil':
-        palabra_base = random.choice(palabras_faciles)
-    elif nivel == 'intermedio':
-        palabra_base = random.choice(palabras_intermedias)
     else:
-        palabra_base = random.choice(palabras_avanzadas)
-    letras_adivinadas = []
-    print('La palabra base tiene', len(palabra_base), 'letras.')
-    while True:
-        letra = input('Ingresa una letra (o 0 para salir): ').lower()
-        if letra == '0':
-            break
-        if not letra.isalpha() or len(letra) > 1:
-            print('Ingresa una letra valida.')
-            continue
-        if not validar_letra(letra, letras_adivinadas):
-            continue
-        letras_adivinadas.append(letra)
-        palabra_mostrada = mostrar_palabra(palabra_base, letras_adivinadas)
-        print(palabra_mostrada)
-        if '-' not in palabra_mostrada:
-            print('¡Ganaste!')
-            break
-    print('Fin del juego.')
+        print("Invalid choice, try again.")
+        return special_ability_menu(player)
 
-# Ejecutamos el juego
-jugar_ahorcado()
+def battle_menu(player, enemy):
+    print("Battle options:")
+    print("1. Attack")
+    print("2. Defend")
+    print("3. Use special ability")
+    print("4. Use potion")
+    print("5. Flee")
+
+    choice = int(input())
+
+    if choice == 1:
+        damage = max(0, player.attack - enemy.defense)
+        enemy.take_damage(damage)
+        print(f"You dealt {damage} damage to the {enemy.name}!")
+    elif choice == 2:
+        print("You brace yourself for the next attack.")
+    elif choice == 3:
+        if special_ability_menu(player):
+            if isinstance(player, Mage):
+                player.cast_spell(enemy)
+            elif isinstance(player, Knight):
+                player.fortify()
+            elif isinstance(player, Assassin):
+                player.critical_strike(enemy)
+            elif isinstance(player, Tank):
+                player.provoke(enemy)
+    elif choice == 4:
+        print("You drink a potion.")
+        player.health += 30
+        player.attack += 5
+        player.defense += 5
+    elif choice == 5:
+        print("You attempt to flee...")
+        if random.random() < 0.5:
+            print("You escaped!")
+            return True
+        else:
+            print("You failed to flee.")
+    else:
+        print("Invalid choice, try again.")
+        return battle_menu(player, enemy)
+
+    return False
+
+def enemy_turn(player, enemy):
+    damage = max(0, enemy.attack - player.defense)
+    player.take_damage(damage)
+    print(f"The {enemy.name} dealt {damage} damage to you!")
+
+def main():
+    print("Welcome to the RPG game!")
+
+player = choose_character()
+print(f"You have chosen the {player.name}.")
+print_stats(player)
+
+while True:
+    enemy = create_enemy(player.level)
+    print(f"A wild {enemy.name} has appeared!")
+    print_stats(enemy)
+
+    while not player.is_dead() and not enemy.is_dead():
+        if battle_menu(player, enemy):
+            break
+
+        if enemy.is_dead():
+            print(f"You have defeated the {enemy.name}!")
+            player.gain_exp(enemy.level * 50)
+        else:
+            enemy_turn(player, enemy)
+
+            if player.is_dead():
+                print(f"You havebeen defeated by the {enemy.name}.")
+                print("Game over.")
+
+    print_stats(player)
+
+    if player.is_dead():
+        break
+
+    print("Do you want to continue? (yes/no)")
+    continue_game = input().lower()
+    if continue_game == "no":
+        print("Thank you for playing!")
+        break
+
+if __name__ == "__main__":
+    main()
